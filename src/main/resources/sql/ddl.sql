58c8feeda0493b2296083686233aeafa2f0be73b
Pickle <523837193@qq.com>
2020-09-09 17:16:27 +0800

    update .gitignore

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -4,44 +4,44 @@ CREATE DATABASE `wechat_public_personal` DEFAULT CHARACTER SET utf8mb4 COLLATE u
 
 create table city
 (
-  id        bigint(255) auto_increment
+  id        bigint(127) auto_increment
     primary key,
-  city_code varchar(255) null,
-  city_name varchar(255) null,
+  city_code varchar(127) null,
+  city_name varchar(127) null,
   constraint city_code
   unique (city_code, city_name)
 );
 
 create table log
 (
-  id        bigint(255) auto_increment
+  id        bigint(127) auto_increment
     primary key,
-  user_name varchar(32)  null,
-  message   varchar(255) null,
+  user_name varchar(31)  null,
+  message   varchar(127) null,
   time      datetime     null
 );
 
 create table sentence
 (
-  id          bigint(255) auto_increment
+  id          bigint(127) auto_increment
     primary key,
-  content     varchar(255) null,
-  note        varchar(255) null,
-  translation varchar(255) null,
-  date        varchar(255) null
+  content     varchar(127) null,
+  note        varchar(127) null,
+  translation varchar(127) null,
+  date        varchar(127) null
 );
 
 create table weather
 (
   id           bigint auto_increment
     primary key,
-  cityCode     varchar(255) null,
-  date         varchar(255) null,
-  dayWeather   varchar(255) null,
-  nightWeather varchar(255) null,
-  dayTemp      varchar(255) null,
-  nightTemp    varchar(255) null,
-  windLevel    varchar(255) null,
+  cityCode     varchar(127) null,
+  date         varchar(127) null,
+  dayWeather   varchar(127) null,
+  nightWeather varchar(127) null,
+  dayTemp      varchar(127) null,
+  nightTemp    varchar(127) null,
+  windLevel    varchar(127) null,
   constraint cityCode
   unique (cityCode, date)
 );
